d38e9255f3f6e410072125a442f39b014277d83a
Matthew Thompson <commanderblop@gmail.com>
2015-10-20 16:55:02 +0100

    Basic parsing of TropICAL works.

--- src/tropic/core.clj
+++ src/tropic/core.clj
@@ -1,52 +1,46 @@
 (ns tropic.core
   (:require [instaparse.core :as insta]))
 
-(def inform
+(def tropical
   (insta/parser
-   "rule =
-        <'Definition: ' ('a ' / 'an ')> kind <' is '> adjective <' '> ('if' / 'unless') <' '> definition
-        | preamble <': '> phrases
-
-    definition =
-        condition
-        | <('its' / 'his' / 'her' / 'their') ' '> property <' ' ('is' / 'are') ' '> value <' '> 'or' <' '> ('less' / 'more')
-        | ': ' phrases
-
-    preamble =
-        'To' <' '> phrases
-        | 'To decide ' ('if' / 'whether') ' ' phrases
-        | 'To decide ' ('which' / 'what') ' ' phrases
-        | 'This is the ' word
-
-    circumstances =
-        'At ' time
-        | 'When ' event
-        | [placement] rulebook-reference [' while ' / ' when ' condition] [' during ' scene]
-
-    rulebook-reference =
-        word
-
-    placement =
-        ('a' / 'an')
-        | ['the '] 'first'
-        | ['the '] 'last'
-
-    scene =
-        word
-
-    phrases = phrase+
-    phrase = word | word <' '>
-    kind = word
-    time = word
-    event = word
-    property = word
-    value = word
-    condition = word
-    adjective = word
-    word = #'[0-9a-zA-Z\\-\\_\\.]*'"))
-
-;; (inform "To exit: go north")
-;; (inform "Definition: a toad is ugly if warty")
-;; (inform "Definition: a toad is dying if its health is 10 or less")
+   "narrative = rule+
+    rule = tropedef | situationdef | taskdef | storydef
+    <tropedef> =
+        tropename (<'\\nIt begins when '> event <'\\n'>) (<'Then '> event <'\\n'>)* (<'It ends when '> event <'\\n'>?)
+    <tropename> =
+        <'The '?> trope <' is a trope' '.'?>
+    <situationdef> = situation <'\\n'> (permission / obligation) [(<'\\n'> (permission / obligation))* (<'\\nFinally, '> (permission / obligation) <'\\n'>?)]
+    situation =
+        <'When '> event
+    event =
+        character <' is '> <'in ' / 'at '> place
+        | character <' gets '> <'a ' / 'an '> task
+        | character <whitespace> task
+    place = <'the '?> name
+    character = <'The ' / 'the '>? name
+    taskdef =
+        taskname <'\\nTo complete it, '> item <' must be '> state <'.'?> <'\\nOtherwise, '> consequence <'.'?> <'\\n'?>
+    taskname =
+        task <' is a task' '.'?>
+    permission = character <' may '> task
+    obligation = character <' must '> task
+    task = verb | verb <(' the ' / ' a ')> item | visit
+    visit = <'leave ' | 'return to ' | 'go to ' | 'visit '> place
+    verb = word
+    place = word
+    consequence = item <' '> verb
+    item = [<'The ' / 'the '>] (word / word <' '> word)
+    state = word
+    <storydef> =
+        storyname
+    <storyname> =
+        story <' is a story' '.'?>
+    story = word
+    trope = words
+    <name> = word
+    <whitespace> = #'\\s+'
+    <words> = word (<whitespace> word)*
+    <word> = #'[0-9a-zA-Z\\-\\_\\']*'"
+))
 
 
--- test/tropic/core_test.clj
+++ test/tropic/core_test.clj
@@ -5,14 +5,39 @@
 (deftest rules
   (testing "Testing rules"
     (testing "no parse errors"
-      (is (vector? (inform "To exit: go north ")))
-      (is (vector? (inform "Definition: a toad is ugly if warty.")))
-      (is (vector? (inform "Definition: a toad is dying if its health is 10 or less")))
-      (is (vector? (inform "To decide if the goblin is behind the door: go left")))
-      (is (vector? (inform "This is the castle: inside lurks a goblin.")))
+      (is (vector? (tropical "The Hero's Journey is a trope")))
+      (is (vector? (tropical "Evil Empire is a trope.")))
+      (is (vector? (tropical "When the hero is at home")))
+      (is (vector? (tropical "When the Wise Sage is in the dell")))
+      (is (vector? (tropical "When Dan is in the Durley Dean")))
+      (is (vector? (tropical "When Gandalf gets a quest")))
+      (is (vector? (tropical "Destroy the Death Star is a task")))
       )))
 
-(inform "To exit: go north")
-(inform "Definition: a toad is ugly if warty")
-(inform "Definition: a toad is dying if its health is 10 or less")
-(inform "To decide if the goblin is behind the door: go left")
+(defn multi [lines]
+  (clojure.string/join "\n" lines))
+
+(tropical (multi [
+                  "The Hero's journey is a trope"
+                  "It begins when the Hero is at home"
+                  "Then the hero gets a quest"
+                  "It ends when the Hero returns"
+                  ;;
+                  "When the Hero gets a quest"
+                  "The Hero must leave home"
+                  "The Hero may sit"
+                  "Finally, the Hero may destroy the Death Star"
+                  ;;
+                  "Destroy the Death Star is a task"
+                  "To complete it, the Death Star must be destroyed"
+                  "Otherwise, the rebel base explodes"
+                  ]))
+(tropical "Evil Empire is a trope")
+(tropical "When the hero is at home")
+(tropical "When the Wise Sage is in the dell")
+(tropical "When Dan is in the Durley Dean")
+(tropical "When Gandalf gets a quest")
+(tropical "When the Wizard gets a quest\n")
+(tropical "Destroy the Death Star is a task")
+
+
