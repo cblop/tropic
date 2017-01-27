d28fffd4d5a2f7711dbbabc6ab463c8dc11f0a29
_mthom <matt@mthompson.org>
2017-01-27 15:22:54 +0000

    Tropes now start as "inactive"

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tropic "0.4.9"
+(defproject tropic "0.5.2"
   :description "DSL for interactive narrative"
   :url "http://mthompson.org/tropical"
   :license {:name "Eclipse Public License"
